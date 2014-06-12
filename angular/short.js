8992478fafedaaccf11eeef62a5e3f993495786c
Juan Pablo Francisconi <juanpablof@gmail.com>
2014-06-11 23:23:21 -0400

    Adding AngularJS - controller & service.

--- angular/short.js
+++ angular/short.js
@@ -1,16 +1,34 @@
+/** Basic AngularJS module */
 var shortener = angular.module('shortener', []);
 
-shortener.service('ShortenerService', function($scope) {
+/** Connects our Single Page App with our Backend [Node] */
+shortener.service('ShortenerService', function($q, $http) {
 
-
-  	
+	this.getShortUrl = function(longUrl){
+		var delay = $q.defer();
+		$http.get('/api/short?url='+longUrl)
+			.success(function(response){delay.resolve(response);})
+			.error(function(){delay.reject("Error getting customer accounts.");});
+		return delay.promise;
+	};
 });
 
+/** Binds variables and functions */
 shortener.controller('ShortenerController', function($scope, ShortenerService) {
-  	var init = function(){
-		console.log('here');
+  	
+  	$scope.submit = function(){
+  		if(typeof $scope.longUrl !== 'undefined'){
+  			ShortenerService.getShortUrl($scope.longUrl).then(function(response){
+					$scope.shortUrl = response;
+			});	
+  		}else{
+  			//error msg
+  		}
   	};
 
-  	init();
+  	$scope.reset = function(){
+  		$scope.longUrl = '';
+  		$scope.shortUrl = '';
+  	};
   	
 });
